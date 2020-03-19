b10dbfea977b69cfa9a59617a00f1147cd63b808
kim-zm <badalice@hanmail.net>
2020-03-19 21:32:02 +0900

    update 20200319

--- src/main/java/com/web/boot/config/SecurityConfig.java
+++ src/main/java/com/web/boot/config/SecurityConfig.java
@@ -1,16 +1,12 @@
 package com.web.boot.config;
 
-import java.util.UUID;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import io.netty.handler.codec.http.HttpMethod;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public SecurityConfig(AdminServerProperties adminServer) {
         this.adminServer = adminServer;
     }
- 
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	String contextPath = this.adminServer.getContextPath();
